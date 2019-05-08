6ce09191d94c1685b35197b40fe0137d7cedcedf
willklundgren <willklundgren@gmail.com>
2019-05-08 00:22:38 -0500

    Product titles appear as a list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const getTitles = (products) => {
+//     products.
+// }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [products, changeProducts] = useState(null);
+
+    const url = '/data/products.json';
+
+    React.useEffect( () => {
+
+        const getProducts = async () => {
+            const response = await fetch(url);
+            if (!response.ok) throw response;
+            const json = await response.json();
+            changeProducts(json);
+        };
+
+        getProducts();
+
+    }, []);
+
+    console.log("Products object is...")
+    console.log(products);
+    if (products != null) {
+        console.log( products['products'] );
+    }
+
+    var titles = [];
+
+    if (products != null) {
+        for (var product in products['products']) {
+            titles.push(products['products'][product]['title']);
+        }
+    };
+
+    console.log(titles)
+    
+
+    return (
+        <div>
+            {titles.map( (title) => <li>{title}</li>)}
+        </div>
+    );
 }
 
 export default App;
